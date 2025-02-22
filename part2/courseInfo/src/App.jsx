78d3c730f3c0f52056e141db82a6291fe925e187
brandmoredev <brandmoredev@gmail.com>
2025-02-22 20:12:11 +0300

    Exercise 2.5 - Extracts course into its own module

--- part2/courseInfo/src/App.jsx
+++ part2/courseInfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return parts.map((part, index) => {
-    return <Part part={part.name} exercises={part.exercises} key={index}/>
-  })
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <p><strong>Number of exercises {total}</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
