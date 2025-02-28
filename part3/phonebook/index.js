99956962e369ed44d98940fa7e634a46d30e7e61
brandmoredev <brandmoredev@gmail.com>
2025-02-28 22:55:48 +0300

    Exercise 3.5-3.6 - Adds new data and error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -61,6 +61,38 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 })
 
+
+// POST NEW PHONEBOOK ENTRY
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  if (body.name && phonebook.find(person => person.name.toLocaleLowerCase() === body.name.toLocaleLowerCase())) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(person)
+
+  response.json(person);
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
