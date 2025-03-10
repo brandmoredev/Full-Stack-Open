bc33cba09bccf7e46ddeca9d8d52ad607d288525
Brando <brandmoredev@gmail.com>
2025-03-10 21:20:40 +0300

    Adds frontend static site to backend

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const cors = require('cors');
 
+app.use(express.static('dist'));
 app.use(express.json());
-app.use(cors());
 app.use(morgan('tiny'));
 
 let phonebook = [
