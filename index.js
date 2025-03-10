6f058f73bda6be949a4a2b540d6a61c59ae0f19a
Brando <brandmoredev@gmail.com>
2025-03-10 20:17:49 +0300

    Isolatin phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors());
 app.use(morgan('tiny'));
 
 let phonebook = [
@@ -108,7 +110,13 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 })
 
-const PORT = 3001;
+const PORT = process.env.port | 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
