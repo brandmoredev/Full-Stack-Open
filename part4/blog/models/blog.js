4852ff93240f6837b078b9f28f1bba6716b08ef0
Brando <brandmoredev@gmail.com>
2025-04-05 04:25:27 -0700

    Adds validation on blog model

--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
+  title: {
+    type: String,
+    minLength: 10,
+    required: true
+  },
+  author: {
+    type: String,
+    minLength: 5,
+    require: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
   likes: Number
 })
 
