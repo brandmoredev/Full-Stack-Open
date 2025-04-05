8328c796c59c697cddd33fb1d7d31eba8b9da451
Brando <brandmoredev@gmail.com>
2025-04-05 03:53:50 -0700

    Start Exercise 4.2 - extracts model blog to own folder

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,16 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-console.log('TEST')
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 
 const mongoUrl = process.env.MONGODB_URI
