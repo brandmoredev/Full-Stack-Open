82c1d226bbfd0e84a0bbbc48813ebd33fa33f704
Brando <brandmoredev@gmail.com>
2025-04-05 04:00:51 -0700

    Extracts controller to its own module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blog')
 
 
 const mongoUrl = process.env.MONGODB_URI
@@ -13,25 +13,8 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  console.log('TEST')
-
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
