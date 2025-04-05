ff80d585b39fbffaba5679a7d3cb664675ba09a7
Brando <brandmoredev@gmail.com>
2025-04-05 04:40:24 -0700

    Completes Exercise 4.2 - Extracts app into its own folder

---
+++ part4/blog/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,26 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blog')
-const middleware = require('./utils/middleware')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
