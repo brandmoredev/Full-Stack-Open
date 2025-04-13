11793c6d10a6fa198c370c43c4f83e1192177089
Brando <brandmoredev@gmail.com>
2025-04-13 21:34:22 +0300

    Exercise 4.8 - Blog list test /api/blogs

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,35 @@
+const { test, beforeEach, after } = require('node:test')
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+const helper = require('../tests/test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
