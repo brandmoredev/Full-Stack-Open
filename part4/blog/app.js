bf5c549f5bcb26452da6f45fb6c6f5ff140d8ea2
Brando <brandmoredev@gmail.com>
2025-05-09 22:27:45 +0300

    Exercise 4.15 - add users

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,6 +7,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
@@ -25,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part4/blog/controllers/user.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    password: hashedPassword,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
+
